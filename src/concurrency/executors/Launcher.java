a3723a2fffc2306cc2e1fe28b2bb06ae08fdde4e
Vadym Koroliuk W <vkqa@outlook.com>
2019-08-19 12:54:01 -0700

    Tread next

--- src/concurrency/executors/Launcher.java
+++ src/concurrency/executors/Launcher.java
@@ -1,25 +1,58 @@
 package concurrency.executors;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
 import static concurrency.threadslesson.ColorSheme.RED;
 
 public class Launcher {
     private static final int POOL_SIZE = 2;
 
     public static void main(String[] args) throws InterruptedException {
-        boolean isDaemon = false;
+        boolean isDaemon = true;
         System.out.println(RED + "Starting main thread");
         GCDRunnable r = new GCDRunnable(isDaemon);
-        runInOneThread(r, isDaemon);
-        Thread.sleep(1500);
+//        runInOneThread(r, isDaemon);
+        runWithExecutors(r, isDaemon);
         System.out.println(RED + "Leaving the main thread");
     }
+
     private static void runInOneThread(GCDRunnable r, boolean isDaemon) throws InterruptedException {
         Thread th = new Thread(r);
-        if (isDaemon){
+        if (isDaemon) {
             th.setDaemon(true);
         }
         th.start();
 //        Thread.sleep(100);
 //        th.interrupt();
     }
+
+    private static void runWithExecutors(GCDRunnable r, boolean isDaemom) throws InterruptedException {
+
+        ThreadFactory factory = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                if (isDaemom) {
+                    thread.setDaemon(true);
+                }
+                return thread;
+            }
+        };
+
+
+    ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE, factory);
+        for(
+    int i = 0; i<5;i++)
+
+    {
+        executorService.execute(r);
+    }
+        executorService.shutdown();
+
+        executorService.awaitTermination(30,TimeUnit.SECONDS);
+
+}
 }
